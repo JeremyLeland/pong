fb54e5d9b43b115dfac047c1b53a3606de403c40
Jeremy Leland <jeremy.leland@gmail.com>
2021-12-28 13:40:07 -0800

    Include radius, but make sure hitX,Y is within line

--- src/Segment.js
+++ src/Segment.js
@@ -8,30 +8,50 @@ export class Segment {
     this.midX = ( x1 + x2 ) / 2;
     this.midY = ( y1 + y2 ) / 2;
 
-    const len = Math.hypot( x2 - x1, y2 - y1 );
-    this.normalX = ( y2 - y1 ) / len;
-    this.normalY = ( x1 - x2 ) / len;
+    this.length = Math.hypot( x2 - x1, y2 - y1 );
+    this.normalX = ( y2 - y1 ) / this.length;
+    this.normalY = ( x1 - x2 ) / this.length;
   }
 
-  getCollision( entity, dt ) {
+  getCollision( entity ) {
 
-    // See http://www.jeffreythompson.org/collision-detection/line-line.php
+    const distFromLine = ( this.x1 - entity.x ) * this.normalX + ( this.y1 - entity.y ) * this.normalY;
 
-    const x1 = this.x1, y1 = this.y1, x2 = this.x2, y2 = this.y2;
-    const x3 = entity.x, y3 = entity.y;
-    const x4 = entity.x + entity.dx * dt;
-    const y4 = entity.y + entity.dy * dt;
+    const vDotN = entity.dx * this.normalX + entity.dy * this.normalY;
+    const hitTime = ( distFromLine + entity.radius ) / vDotN;
 
-    const dn = ( ( y4 - y3 ) * ( x2 - x1 ) - ( x4 - x3 ) * ( y2 - y1 ) );
-    const uA = ( ( x4 - x3 ) * ( y1 - y3 ) - ( y4 - y3 ) * ( x1 - x3 ) ) / dn;
-    const uB = ( ( x2 - x1 ) * ( y1 - y3 ) - ( y2 - y1 ) * ( x1 - x3 ) ) / dn;
+    const hitX = entity.x + entity.dx * hitTime;
+    const hitY = entity.y + entity.dy * hitTime;
 
-    // TODO: Incorporate radius
-    return {
-      time: 0 <= uA && uA <= 1 /* && 0 <= uB && uB <= 1 */ ? uB : Infinity,
-      normalX: this.normalX,
-      normalY: this.normalY,
-    };
+    const d1 = Math.hypot( this.x1 - hitX, this.y1 - hitY );
+    const d2 = Math.hypot( this.x2 - hitX, this.y2 - hitY );
+    
+
+    // // See http://www.jeffreythompson.org/collision-detection/line-line.php
+
+    // const x1 = this.x1, y1 = this.y1, x2 = this.x2, y2 = this.y2;
+    // const x3 = entity.x, y3 = entity.y;
+    // const x4 = entity.x + entity.dx * dt;
+    // const y4 = entity.y + entity.dy * dt;
+
+    // const dn = ( ( y4 - y3 ) * ( x2 - x1 ) - ( x4 - x3 ) * ( y2 - y1 ) );
+    // const uA = ( ( x4 - x3 ) * ( y1 - y3 ) - ( y4 - y3 ) * ( x1 - x3 ) ) / dn;
+    // const uB = ( ( x2 - x1 ) * ( y1 - y3 ) - ( y2 - y1 ) * ( x1 - x3 ) ) / dn;
+
+    if ( d1 < this.length && d2 < this.length ) {
+      return {
+        time: hitTime,
+        normalX: this.normalX,
+        normalY: this.normalY,
+      };
+    }
+    else {
+      return {
+        time: Infinity,
+        normalX: 0,
+        normalY: 0,
+      };
+    }
   }
 
   draw( ctx ) {
