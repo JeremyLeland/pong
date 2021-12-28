ec3d1832fc36e43f2d55216c7eba30ca16e0d5a8
Jeremy Leland <jeremy.leland@gmail.com>
2021-12-28 11:00:05 -0800

    Only count collisions in segment
    Need to incorporate radius now somehow

--- src/Segment.js
+++ src/Segment.js
@@ -14,12 +14,21 @@ export class Segment {
   }
 
   getCollision( entity, dt ) {
-    const distFromLine = (this.x1 - entity.x) * this.normalX + (this.y1 - entity.y) * this.normalY;
 
-    const vDotN = entity.dx * dt * this.normalX + entity.dy * dt * this.normalY;
+    // See http://www.jeffreythompson.org/collision-detection/line-line.php
 
+    const x1 = this.x1, y1 = this.y1, x2 = this.x2, y2 = this.y2;
+    const x3 = entity.x, y3 = entity.y;
+    const x4 = entity.x + entity.dx * dt;
+    const y4 = entity.y + entity.dy * dt;
+
+    const dn = ( ( y4 - y3 ) * ( x2 - x1 ) - ( x4 - x3 ) * ( y2 - y1 ) );
+    const uA = ( ( x4 - x3 ) * ( y1 - y3 ) - ( y4 - y3 ) * ( x1 - x3 ) ) / dn;
+    const uB = ( ( x2 - x1 ) * ( y1 - y3 ) - ( y2 - y1 ) * ( x1 - x3 ) ) / dn;
+
+    // TODO: Incorporate radius
     return {
-      time: ( distFromLine + entity.radius ) / vDotN,   // TODO: Sometimes -? Properly incorporate radius
+      time: 0 <= uA && uA <= 1 /* && 0 <= uB && uB <= 1 */ ? uB : Infinity,
       normalX: this.normalX,
       normalY: this.normalY,
     };
