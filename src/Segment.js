844912b322ac23d6c19c1e269361a44addf52a80
Jeremy Leland <jeremy.leland@gmail.com>
2021-12-28 20:07:44 -0800

    Detect ends of line segments and bounce appropriately

--- src/Segment.js
+++ src/Segment.js
@@ -25,20 +25,7 @@ export class Segment {
 
     const d1 = Math.hypot( this.x1 - hitX, this.y1 - hitY );
     const d2 = Math.hypot( this.x2 - hitX, this.y2 - hitY );
-    
-
-    // // See http://www.jeffreythompson.org/collision-detection/line-line.php
-
-    // const x1 = this.x1, y1 = this.y1, x2 = this.x2, y2 = this.y2;
-    // const x3 = entity.x, y3 = entity.y;
-    // const x4 = entity.x + entity.dx * dt;
-    // const y4 = entity.y + entity.dy * dt;
-
-    // const dn = ( ( y4 - y3 ) * ( x2 - x1 ) - ( x4 - x3 ) * ( y2 - y1 ) );
-    // const uA = ( ( x4 - x3 ) * ( y1 - y3 ) - ( y4 - y3 ) * ( x1 - x3 ) ) / dn;
-    // const uB = ( ( x2 - x1 ) * ( y1 - y3 ) - ( y2 - y1 ) * ( x1 - x3 ) ) / dn;
-
-    
+        
     // Inside segment
     if ( d1 < this.length && d2 < this.length ) {
       return {
@@ -48,53 +35,11 @@ export class Segment {
       };
     }
 
-    // First point
-    // See: https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm
-    const dX = entity.dx;
-    const dY = entity.dy;
-    const fX = entity.x - this.x1;
-    const fY = entity.y - this.y1;
+    // End points. If these miss, they will return time: Infinity
+    const hit1 = getPointHit( entity, this.x1, this.y1 );
+    const hit2 = getPointHit( entity, this.x2, this.y2 );
 
-    const a = dX * dX + dY * dY;
-    const b = 2 * ( fX * dX + fY * dY ); 
-    const c = ( fX * fX + fY * fY ) - Math.pow( entity.radius, 2 );
-
-    let disc = b * b - 4 * a * c;
-
-    if ( disc > 0 ) {
-      disc = Math.sqrt( disc );
-
-      const t0 = ( -b - disc ) / ( 2 * a );
-      const t1 = ( -b + disc ) / ( 2 * a );
-
-      const hitTime = t0;// < 0 ? t1 : t0;
-
-      const hitX = entity.x + entity.dx * hitTime;
-      const hitY = entity.y + entity.dy * hitTime;
-
-      let nx = this.x1 - hitX;
-      let ny = this.y1 - hitY;
-      const len = Math.hypot( nx, ny );
-      nx /= len;
-      ny /= len;
-
-      return {
-        time: hitTime,
-        normalX: nx,
-        normalY: ny,
-      }
-    }
-    
-    // Second point
-    // else if ( d2 < entity.radius ) {
-
-    // }
-    // Complete miss
-    return {
-      time: Infinity,
-      normalX: 0,
-      normalY: 0,
-    };
+    return hit1.time < hit2.time ? hit1 : hit2;
   }
 
   draw( ctx ) {
@@ -109,5 +54,48 @@ export class Segment {
 
     ctx.strokeStyle = 'white';
     ctx.stroke();
+  }
+}
+
+function getPointHit( entity, cx, cy ) {
+  // See: https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm
+  const dX = entity.dx;
+  const dY = entity.dy;
+  const fX = entity.x - cx;
+  const fY = entity.y - cy;
+
+  const a = dX * dX + dY * dY;
+  const b = 2 * ( fX * dX + fY * dY ); 
+  const c = ( fX * fX + fY * fY ) - Math.pow( entity.radius, 2 );
+
+  let disc = b * b - 4 * a * c;
+
+  if ( disc > 0 ) {
+    disc = Math.sqrt( disc );
+
+    const t0 = ( -b - disc ) / ( 2 * a );
+    //const t1 = ( -b + disc ) / ( 2 * a );
+
+    const hitTime = t0;// < 0 ? t1 : t0;
+
+    const hitX = entity.x + entity.dx * hitTime;
+    const hitY = entity.y + entity.dy * hitTime;
+
+    let nx = cx - hitX;
+    let ny = cy - hitY;
+    const len = Math.hypot( nx, ny );
+    nx /= len;
+    ny /= len;
+
+    return {
+      time: hitTime,
+      normalX: nx,
+      normalY: ny,
+    }
+  }
+  else {
+    return {
+      time: Infinity
+    }
   }
 }
